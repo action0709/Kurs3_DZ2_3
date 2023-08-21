f501a0b626dbeefc755607f0e8b05407782e56a1
action0709 <action0709@mail.ru>
2023-08-21 19:20:13 +0400

    4

--- src/main/java/com/SkyPro/Kurs3_DZ2_3/service/AvatarService.java
+++ src/main/java/com/SkyPro/Kurs3_DZ2_3/service/AvatarService.java
@@ -4,6 +4,8 @@ import com.SkyPro.Kurs3_DZ2_3.repository.AvatarRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 @Service
@@ -17,6 +19,10 @@ private Path pathToAvatars;
     }
 
     public  Long save(Long StudentId, MultipartFile multipartFile){
+        Files.createDirectories(pathToAvatars);
+        String  originalFilename= multipartFile.getOriginalFilename();
+        int dotIndex=originalFilename.lastIndexOf(".");
+        String extension=originalFilename.substring(dotIndex);
 
     }
 }
