a2630fc2e56a95e7be973c869f24ae6b7a748085
action0709 <action0709@mail.ru>
2023-08-28 20:25:56 +0400

    Test-reat-template1

--- src/test/java/com/SkyPro/Kurs3_DZ2_3/controller/FacultyControllerTest.java
+++ src/test/java/com/SkyPro/Kurs3_DZ2_3/controller/FacultyControllerTest.java
@@ -3,6 +3,7 @@ package com.SkyPro.Kurs3_DZ2_3.controller;
 import com.SkyPro.Kurs3_DZ2_3.Kurs3Dz23Application;
 import com.SkyPro.Kurs3_DZ2_3.model.Faculty;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
@@ -13,7 +14,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 @SpringBootTest(classes = Kurs3Dz23Application.class,
 webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT )
 public class FacultyControllerTest {
-
+@Autowired
     TestRestTemplate template;
     @Test
     void create(){
