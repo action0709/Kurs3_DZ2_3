594b8ebcfc7bd208385e9e01ab1a7bbe46db37c8
action0709 <action0709@mail.ru>
2023-08-29 17:07:16 +0400

    web-mvc-test7

---
+++ src/test/java/com/SkyPro/Kurs3_DZ2_3/controller/FacultyControllerTest.java
@@ -0,0 +1,54 @@
+package com.SkyPro.Kurs3_DZ2_3.controller;
+
+import com.SkyPro.Kurs3_DZ2_3.model.Faculty;
+import com.SkyPro.Kurs3_DZ2_3.model.Student;
+import com.SkyPro.Kurs3_DZ2_3.repository.FacultyRepository;
+import com.SkyPro.Kurs3_DZ2_3.repository.StudentRepository;
+import com.SkyPro.Kurs3_DZ2_3.service.FacultyService;
+import com.SkyPro.Kurs3_DZ2_3.service.StudentService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(FacultyController.class)
+public class FacultyControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+    @Autowired
+    ObjectMapper objectMapper;
+    @MockBean
+    StudentRepository studentRepository;
+    @MockBean
+    FacultyRepository facultyRepository;
+    @SpyBean
+    FacultyService facultyService;
+
+    @Test
+    void getById() throws Exception {
+        Faculty faculty = new Faculty(1L, "filfak", "blue");
+        when(facultyRepository.findById(1L)).thenReturn(Optional.of(faculty));
+        mockMvc.perform(MockMvcRequestBuilders.get("/faculty/1")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("filfak"))
+                .andExpect(jsonPath("$.color").value("blue"));
+    }
+
+}
+
