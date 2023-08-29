e89f0c5b2d80c69e07405ba5585f68fd3b137c8b
action0709 <action0709@mail.ru>
2023-08-29 13:26:59 +0400

    web-mvc-test

---
+++ src/test/java/com/SkyPro/Kurs3_DZ2_3/controller/StudentControllerTest.java
@@ -0,0 +1,42 @@
+package com.SkyPro.Kurs3_DZ2_3.controller;
+
+import com.SkyPro.Kurs3_DZ2_3.model.Student;
+import com.SkyPro.Kurs3_DZ2_3.repository.FacultyRepository;
+import com.SkyPro.Kurs3_DZ2_3.repository.StudentRepository;
+import com.SkyPro.Kurs3_DZ2_3.service.StudentService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(StudentController.class)
+public class StudentControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+    @MockBean
+    StudentRepository studentRepository;
+    @MockBean
+    FacultyRepository facultyRepository;
+    @SpyBean
+    StudentService studentService;
+
+    @Test
+    void getById(){
+        Student student = new Student(1L, "petr", "25");
+        when(studentRepository.findById()).thenReturn(Optional.of(student));
+        mockMvc.perform(MockMvcRequestBuilders.get("/student/1")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+    }
+}
