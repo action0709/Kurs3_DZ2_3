6e23a364fc2cbef2775acd27da3a89342456887d
action0709 <action0709@mail.ru>
2023-09-18 20:41:07 +0400

    DZ5_4

--- src/main/java/com/SkyPro/Kurs3_DZ2_3/controller/InfoController.java
+++ src/main/java/com/SkyPro/Kurs3_DZ2_3/controller/InfoController.java
@@ -1,19 +1,29 @@
 package com.SkyPro.Kurs3_DZ2_3.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.stream.Stream;
+
 @RestController
 @RequestMapping("/info")
 public class InfoController {
+    private static final Logger logger = LoggerFactory.getLogger(InfoController.class);
 @Value("${server.port}")
     private  int port;
-
-
     @GetMapping("get-port")
     public int getPort(){
        return port;
    }
+    @GetMapping("/calculate")
+public void calculate(){
+
+       Stream.iterate(1, a -> a + 1)
+               .limit(1_000_000)
+               .reduce(0, (a, b) -> a + b);
+    }
 }
